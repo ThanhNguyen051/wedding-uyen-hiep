6cd4adb2244c8c8c9bfada86178ee203a0576aae
ThanhNguyen051 <huynhthanhnguyenct2001@gmail.com>
2026-08-20 03:31:10 +0700

    test

--- src/app/component/invitation/infomation/infomation.js
+++ src/app/component/invitation/infomation/infomation.js
@@ -51,6 +51,59 @@ function Infomation({
             transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s",
           }}
         >
+          {"08:00"}
+        </span>
+      </p>
+
+      <div className={cx("address")}>{"Giáo xứ Thiên Ân"}</div>
+      <a className={cx("btn", "ani-btn")} href={`tel:${phone}`}>
+        <FaPhone className={cx("icon")} />
+        {phone}
+      </a>
+      <a
+        className={cx("btn")}
+        href={`http://maps.google.com/?q=${street}`}
+        target="_blank"
+      >
+        <FaMapMarkerAlt className={cx("icon")} />
+        Đường đi
+      </a>
+    </div>
+  );
+
+  return (
+    <div className={cx("wrapper")} ref={boxRef}>
+      <House className={cx("house")} />
+
+      <p className={cx("date")}>
+        <span
+          className={cx("date-detail")}
+          style={{
+            transform: boxInView ? "none" : "translateY(150px)",
+            opacity: boxInView ? 1 : 0,
+            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+        >
+          {time.date}
+        </span>
+        <span
+          className={cx("date-detail")}
+          style={{
+            transform: boxInView ? "none" : "translateY(150px)",
+            opacity: boxInView ? 1 : 0,
+            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
+          }}
+        >
+          {time.year}
+        </span>
+        <span
+          className={cx("date-detail")}
+          style={{
+            transform: boxInView ? "none" : "translateY(150px)",
+            opacity: boxInView ? 1 : 0,
+            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s",
+          }}
+        >
           {time.time}
         </span>
       </p>
