5985489f32596efb6e3c4888f9d172b35df1327c
ThanhNguyen051 <huynhthanhnguyenct2001@gmail.com>
2026-08-20 03:37:03 +0700

    test

--- src/app/component/invitation/infomation/infomation.js
+++ src/app/component/invitation/infomation/infomation.js
@@ -19,107 +19,109 @@ function Infomation({
   const boxRef = useRef();
   const boxInView = useInView(boxRef, { once: true });
   return (
-    <div className={cx("wrapper")} ref={boxRef}>
-      <House className={cx("house")} />
+    <>
+      <div className={cx("wrapper")} ref={boxRef}>
+        <House className={cx("house")} />
 
-      <p className={cx("date")}>
-        <span
-          className={cx("date-detail")}
-          style={{
-            transform: boxInView ? "none" : "translateY(150px)",
-            opacity: boxInView ? 1 : 0,
-            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-          }}
-        >
-          {time.date}
-        </span>
-        <span
-          className={cx("date-detail")}
-          style={{
-            transform: boxInView ? "none" : "translateY(150px)",
-            opacity: boxInView ? 1 : 0,
-            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
-          }}
-        >
-          {time.year}
-        </span>
-        <span
-          className={cx("date-detail")}
-          style={{
-            transform: boxInView ? "none" : "translateY(150px)",
-            opacity: boxInView ? 1 : 0,
-            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s",
-          }}
-        >
-          {"08:00"}
-        </span>
-      </p>
+        <p className={cx("date")}>
+          <span
+            className={cx("date-detail")}
+            style={{
+              transform: boxInView ? "none" : "translateY(150px)",
+              opacity: boxInView ? 1 : 0,
+              transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            }}
+          >
+            {time.date}
+          </span>
+          <span
+            className={cx("date-detail")}
+            style={{
+              transform: boxInView ? "none" : "translateY(150px)",
+              opacity: boxInView ? 1 : 0,
+              transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
+            }}
+          >
+            {time.year}
+          </span>
+          <span
+            className={cx("date-detail")}
+            style={{
+              transform: boxInView ? "none" : "translateY(150px)",
+              opacity: boxInView ? 1 : 0,
+              transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s",
+            }}
+          >
+            {"08:00"}
+          </span>
+        </p>
 
-      <div className={cx("address")}>{"Giáo xứ Thiên Ân"}</div>
-      <a className={cx("btn", "ani-btn")} href={`tel:${phone}`}>
-        <FaPhone className={cx("icon")} />
-        {phone}
-      </a>
-      <a
-        className={cx("btn")}
-        href={`http://maps.google.com/?q=${street}`}
-        target="_blank"
-      >
-        <FaMapMarkerAlt className={cx("icon")} />
-        Đường đi
-      </a>
-    </div>
-  
-    <div className={cx("wrapper")} ref={boxRef}>
-      <House className={cx("house")} />
+        <div className={cx("address")}>{"Giáo xứ Thiên Ân"}</div>
+        <a className={cx("btn", "ani-btn")} href={`tel:${phone}`}>
+          <FaPhone className={cx("icon")} />
+          {phone}
+        </a>
+        <a
+          className={cx("btn")}
+          href={`http://maps.google.com/?q=${street}`}
+          target="_blank"
+        >
+          <FaMapMarkerAlt className={cx("icon")} />
+          Đường đi
+        </a>
+      </div>
+    
+      <div className={cx("wrapper")} ref={boxRef}>
+        <House className={cx("house")} />
 
-      <p className={cx("date")}>
-        <span
-          className={cx("date-detail")}
-          style={{
-            transform: boxInView ? "none" : "translateY(150px)",
-            opacity: boxInView ? 1 : 0,
-            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-          }}
-        >
-          {time.date}
-        </span>
-        <span
-          className={cx("date-detail")}
-          style={{
-            transform: boxInView ? "none" : "translateY(150px)",
-            opacity: boxInView ? 1 : 0,
-            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
-          }}
-        >
-          {time.year}
-        </span>
-        <span
-          className={cx("date-detail")}
-          style={{
-            transform: boxInView ? "none" : "translateY(150px)",
-            opacity: boxInView ? 1 : 0,
-            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s",
-          }}
-        >
-          {time.time}
-        </span>
-      </p>
+        <p className={cx("date")}>
+          <span
+            className={cx("date-detail")}
+            style={{
+              transform: boxInView ? "none" : "translateY(150px)",
+              opacity: boxInView ? 1 : 0,
+              transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            }}
+          >
+            {time.date}
+          </span>
+          <span
+            className={cx("date-detail")}
+            style={{
+              transform: boxInView ? "none" : "translateY(150px)",
+              opacity: boxInView ? 1 : 0,
+              transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
+            }}
+          >
+            {time.year}
+          </span>
+          <span
+            className={cx("date-detail")}
+            style={{
+              transform: boxInView ? "none" : "translateY(150px)",
+              opacity: boxInView ? 1 : 0,
+              transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s",
+            }}
+          >
+            {time.time}
+          </span>
+        </p>
 
-      <div className={cx("address")}>{address}</div>
-      <a className={cx("btn", "ani-btn")} href={`tel:${phone}`}>
-        <FaPhone className={cx("icon")} />
-        {phone}
-      </a>
-      <a
-        className={cx("btn")}
-        href={`http://maps.google.com/?q=${street}`}
-        target="_blank"
-      >
-        <FaMapMarkerAlt className={cx("icon")} />
-        Đường đi
-      </a>
-    </div>
+        <div className={cx("address")}>{address}</div>
+        <a className={cx("btn", "ani-btn")} href={`tel:${phone}`}>
+          <FaPhone className={cx("icon")} />
+          {phone}
+        </a>
+        <a
+          className={cx("btn")}
+          href={`http://maps.google.com/?q=${street}`}
+          target="_blank"
+        >
+          <FaMapMarkerAlt className={cx("icon")} />
+          Đường đi
+        </a>
+      </div>
+    </>
   );
 }
 
